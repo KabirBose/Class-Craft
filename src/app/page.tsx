6946c7de0a80b2a3adee9f1ee82c76e6239d3830
Kabir Bose <kabirbose04@gmail.com>
2024-11-03 12:22:43 -0500

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Page() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>The Ultimate Schedule Builder</h1>
+      <h2>
+        No more frustration when choosing your classes, just a{" "}
+        <span className="highlight-blue">simple</span> and{" "}
+        <span className="highlight-green">responsive</span>{" "}
+        <span className="highlight-red">drag-and-drop</span> interface.
+      </h2>
+    </div>
+  );
 }
